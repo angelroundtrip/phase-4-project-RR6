7c3508141170369b2ea947aeda4ab1a7f92c6fbe
Angel Rosario <arosario.us@gmail.com>
2022-04-01 11:22:18 -0400

    Refining style

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -8,7 +8,6 @@ function LandingPage() {
       <header className="landing" style={{fontSize: '200px', fontWeight: 'bold', color: 'hotpink', textDecoration: 'none'}}>
 
         <span style={{fontFamily: 'cursive'}}>Petsagram</span>
-        <span style={{fontFamily: 'cursive'}}> <a href="/login">Petsagram</a></span>
        
         <div className='ui two buttons'>
            <a href="/signup" style={{padding: '10px', margin: '50px', textDecoration: 'none', fontSize: '50px', fontFamily: 'fantasy'}}> signup </a>
