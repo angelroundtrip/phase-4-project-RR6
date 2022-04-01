975316ccc5e5ed2e8e5ac98a05ab175dbdefd84c
Angel Rosario <arosario.us@gmail.com>
2022-03-31 21:28:51 -0400

    User CRUD checked, works but optimization is needed

--- client/src/Components/Posts.js
+++ client/src/Components/Posts.js
@@ -49,7 +49,7 @@ function Posts({posts, setPosts, user, setUser, errors}) {
        
        {user ? <Card> <h3 style={{textAlign:'center'}}>@nekolover</h3> <Image  className="center" style={{align:'center'}} src="https://c.files.bbci.co.uk/151AB/production/_111434468_gettyimages-1143489763.jpg"/> <h3 style={{textAlign:'center'}}>Fun fact: The Japanese word for cat is neko</h3></Card> : null} 
        
-       {user ? <Card> <h3 style={{textAlign:'center'}} style={{textAlign:'center'}}>@Snowflake</h3> <Image  className="center" style={{align:'center'}}  src="https://www.genengnews.com/wp-content/uploads/2021/08/ricky-kharawala-adK3Vu70DEQ-unsplash-scaled-e1628706903421.jpg"/> <h3> I am soft! </h3>   </Card> : null} 
+       {user ? <Card> <h3 style={{textAlign:'center'}} >@Snowflake</h3> <Image  className="center" style={{align:'center'}}  src="https://www.genengnews.com/wp-content/uploads/2021/08/ricky-kharawala-adK3Vu70DEQ-unsplash-scaled-e1628706903421.jpg"/> <h3> I am soft! </h3>   </Card> : null} 
        
       
 
